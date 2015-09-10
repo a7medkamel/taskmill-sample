3099a0dad154d4c8184deb12a66d7888b604fe03
Ahmed Kamel <public@ahmedkamel.net>
2015-09-10 06:01:35 -0700

    update from oncue.io ui

--- hn/who_is_hiring.js
+++ hn/who_is_hiring.js
@@ -20,24 +20,26 @@ module.exports = function(req, res, next) {
     .then(function(data){
       var obj = JSON.parse(data);
       
-      var post_age = (Date.now() - new Date(data.time * 1000)) / 1000;
-
-      Promise
-        .resolve(obj.kids)
-        .map(function(i){
-          return rp
-                  .get('https://hacker-news.firebaseio.com/v0/item/' + i + '.json')
-                  .then(function(data){ return JSON.parse(data); })
-                  .then(function(comment){
-                    return _.pick(comment, 'by', 'id', 'text', 'time');
-                  })
-                  ;
-        })
-        .then(function(jobs){
-          var cache_for = Math.floor(math.eval('60 + (x / (60 * 30)) ^ e + (x / 60)', { x : post_age }));
-          res.set('x-tm-cache-max-age', cache_for);
-          res.send(jobs);
-        });
+      var post_age = (Date.now() - new Date(obj.time * 1000)) / 1000;
+      console.log(post_age);
+      res.end();
+      // Promise
+      //   .resolve(obj.kids)
+      //   .map(function(i){
+      //     return rp
+      //             .get('https://hacker-news.firebaseio.com/v0/item/' + i + '.json')
+      //             .then(function(data){ return JSON.parse(data); })
+      //             .then(function(comment){
+      //               return _.pick(comment, 'by', 'id', 'text', 'time');
+      //             })
+      //             ;
+      //   })
+      //   .then(function(jobs){
+      //     var cache_for = Math.floor(math.eval('60 + (x / (60 * 30)) ^ e + (x / 60)', { x : post_age }));
+          
+      //     res.set('x-tm-cache-max-age', cache_for);
+      //     res.send(jobs);
+      //   });
     })
     .catch(function(err){
       next(err);
